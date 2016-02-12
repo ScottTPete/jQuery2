966808e7aaa7c41d4eb7ee0532897f6e5307df1f
Scott Peterson <stpeterson28@yahoo.com>
2016-02-11 17:06:40 -0700

    Added what what the functions are doing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
+	
 	var listo = [];
+	
+	var Task = function(task){
+		this.task = task;
+		this.id = 'new';
+	};
 	
 	$('#newTaskForm').hide();
 	
-	var advancedTask = function(task) {
+	var advanceTask = function(task) {
 		var modified = task.innerText.trim()
-		for (var i= 0; i <listo.length; i++) {
-			if(listo[i].task === modified) {
-				if(listo[i].id === 'new') {
+		for (var i = 0; i < listo.length; i++) {
+			if (listo[i].task === modified) {
+				if (listo[i].id === 'new') {
 					listo[i].id = 'inProgress';
-				}
-				else if(listo[i].id === 'inProgress') {
+				} else if (listo[i].id === 'inProgress') {
 					listo[i].id = 'archived';
 				} else {
 					listo.splice(i, 1);
@@ -18,57 +23,65 @@ $(document).ready(function(){
 				break;
 			}
 		}
-		task.remove();
-	}
+	task.remove();
+	};
 	
-	$(document).on('click', '#item', function(e){
+// CHANGE TO 'IN PROGRESS'
+	$(document).on('click', '#item', function(e) {
 		e.preventDefault();
-		var task = this;
-		advancedTask(task);
-		this.id = 'inProgress';
+		var task = this;        
+    	advanceTask(task);
+    	this.id = 'inProgress';
 		$('#currentList').append(this.outerHTML);
 	});
+	
+	
+// CHANGE TO 'ARCHIVED'
 	$(document).on('click', '#inProgress', function (e) {
 		e.preventDefault();
 		var task = this;
 		task.id = "archived";
-		var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', "glyphicon-remove");
-		advancedTask(task);
+		var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+		advanceTask(task);
 		$('#archivedList').append(changeIcon);
-	})
-	$(document).on('click', '#archived', function(e) {
-		e.preventDefault();
-		var task = this;
-		advancedTask(task);
 	});
-	var Task = function(task) {
-		this.task = task;
-		this.id = 'new';
-	};
-	var addTask = function(task) {
-		if(task) {
+	
+// DELETE ITEM
+	$(document).on('click', '#archived', function (e) {
+   		e.preventDefault();
+    	var task = this;
+    	advanceTask(task);
+	});
+	
+	var addTask = function(task){
+		if(task){
 			task = new Task(task);
 			listo.push(task);
-		
+			
 			$('#newItemInput').val('');
-		
-			$('#newList').append('<a href="#" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
+			$('#newList').append('<a href="#" class="item-text" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
 		}
-		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
+		$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
 	};
-	$('#saveNewItem').on('click', function(e) {
+	
+	
+	$('#saveNewItem').on('click', function(e){
 		e.preventDefault();
 		var task = $('#newItemInput').val().trim();
 		addTask(task);
 	});
-	//opens form
-	$('#newListItem').on('click', function() {
+	
+	// Opens
+	$('#newListItem').on('click', function(){
 		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
 	});
-	//closes form
+	
+	// Closes
 	$('#cancel').on('click', function(e){
 		e.preventDefault();
 		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
 	});
+	
+
 	
 });
